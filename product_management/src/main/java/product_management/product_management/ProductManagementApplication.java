c4d555994323e72517ac1b91189293a129af9428
sherwin-sayo <sherwin.sayo@mobilemoney.ph>
2024-07-19 15:26:51 +0800

    product_management

---
+++ product_management/src/main/java/product_management/product_management/ProductManagementApplication.java
@@ -0,0 +1,14 @@
+package product_management.product_management;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProductManagementApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementApplication.class, args);
+	}
+
+}
